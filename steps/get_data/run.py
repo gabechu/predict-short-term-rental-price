e9019e3925888cade4a6b56b07372fca8e2b9088
Gabe <wei.g.chu@gmail.com>
2022-12-01 20:11:48 +1100

    added step 1: download data

---
+++ steps/get_data/run.py
@@ -0,0 +1,48 @@
+import logging
+import os
+
+import fire
+import wandb
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def log_artifact(
+    artifact_name, artifact_type, artifact_description, filename, wandb_run
+):
+    artifact = wandb.Artifact(
+        name=artifact_name,
+        type=artifact_type,
+        description=artifact_description,
+    )
+    artifact.add_file(filename)
+    wandb_run.log_artifact(artifact)
+    # We need to call this .wait() method before we can use the
+    # version below. This will wait until the artifact is loaded into W&B and a
+    # version is assigned
+    artifact.wait()
+
+
+def run(sample: str, artifact_name: str, artifact_type: str, artifact_description: str):
+    # this is a bit hacky and must be running at the beginning of the function
+    # other solutions check
+    # https://stackoverflow.com/questions/10724495/getting-all-arguments-and-values-passed-to-a-function
+    args = locals()
+
+    run = wandb.init(job_type="download_file")
+    run.config.update(args)
+
+    logger.info(f"Returning sample {sample}")
+    logger.info(f"Uploading {artifact_name} to Weights & Biases")
+    log_artifact(
+        artifact_name=artifact_name,
+        artifact_type=artifact_type,
+        artifact_description=artifact_description,
+        filename=os.path.join("data", sample),
+        wandb_run=run,
+    )
+
+
+if __name__ == "__main__":
+    fire.Fire(run)
